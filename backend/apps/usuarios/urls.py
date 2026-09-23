3d312ae3b3099d908ef622bae8eed47a4d9f56bd
EduSucari <esucari@unsa.edu.pe>
2026-09-23 16:31:46 -0500

    feat: Agregar nueva ruta en urls.py

--- backend/apps/usuarios/urls.py
+++ backend/apps/usuarios/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from apps.usuarios.views import UsuarioListCreateView
+from apps.usuarios.views import UsuarioListCreateView,  UsuarioRolView
 
 urlpatterns = [
     path("", UsuarioListCreateView.as_view(), name="usuarios"),
+    path("<int:pk>/rol/", UsuarioRolView.as_view(), name="usuario-rol"),
 ]

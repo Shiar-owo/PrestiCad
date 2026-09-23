145d2401ea6cda9c325fc70207f9145143cedfb0
OmarJR18 <oriverao@unsa.edu.pe>
2026-09-23 18:05:04 -0500

    feat(usuarios): expirar sesiones por inactividad

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "apps.usuarios.middleware.ExpiracionSesionInactividadMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -92,6 +93,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CORS_ALLOWED_ORIGINS = [
     o for o in os.getenv("CORS_ALLOWED_ORIGINS", "http://localhost:5173").split(",") if o
 ]
+
+SESSION_IDLE_TIMEOUT_MINUTES = int(os.getenv("SESSION_IDLE_TIMEOUT_MINUTES", "30"))
 
 REST_FRAMEWORK = {
     "DEFAULT_RENDERER_CLASSES": [
